7de805e80fe9d28033718fc39e2c3987d82d09d5
Felipe Cabral <felipe.benites@gmail.com>
2023-07-17 21:54:05 +0200

    First commit

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = """
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+"""
+
+bot_template = """
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+"""
+
+user_template = """
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://1.gravatar.com/avatar/5dbb3b6a237b88cd7dfe181ce72e4ea7a3c090d43ab7dc65d4677e9a93f70e13?size=256">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+"""
